cad7d36dc976b8a9343b20b3107047c3437f8796
ArthurVillela1 <arthur.villela1@gmail.com>
2026-08-15 21:24:23 +0100

    importing data

---
+++ main.py
@@ -0,0 +1,35 @@
+from pandas_datareader import data as web
+import datetime
+
+start = datetime.datetime(2010, 1, 1)
+end = datetime.datetime.today()
+
+series = [
+    "DGS3MO",
+    "DGS6MO",
+    "DGS1",
+    "DGS2",
+    "DGS3",
+    "DGS5",
+    "DGS7",
+    "DGS10",
+    "DGS20",
+    "DGS30"
+]
+
+yields = web.DataReader(series, "fred", start, end)
+
+yields.columns = [
+    "3M",
+    "6M",
+    "1Y",
+    "2Y",
+    "3Y",
+    "5Y",
+    "7Y",
+    "10Y",
+    "20Y",
+    "30Y"
+]
+
+print(yields.tail())
